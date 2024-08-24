a47d6ef05bb6c46021c1eca8e9a6776da8ecbd0f
Rhishav <rhishh.me@gmail.com>
2024-08-24 13:27:52 +0530

    added animation to intro

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
+"use client"
 
-
+import { motion } from "framer-motion"
 import Image from "next/image"
 
 export default function IntroPage() {
@@ -9,19 +10,39 @@ export default function IntroPage() {
         {/* <Image src='logo.svg' width={30} height={30} /> */}
         <p className="text-2xl font-outfit">Livure</p>
       </div>
-      <div>
-        <p className="text-5xl font-outfit mt-36 m-4">Hey</p>
-        <p className="text-4xl font-outfit mt-20 m-4">Welcome to <br/> your personal <br/> medical care vault</p>
-        <p className="text-2xl font-outfit mt-10 ml-4 text-white/40">Your health, our priority</p>
-        <p className="text-2xl font-outfit ml-4 text-white/40">Your journey, our support</p>
-      </div>
-      <div>
+      <motion.div
+        initial={{x:-10,opacity:0}}
+        animate={{x:0,opacity:1}}
+        transition={{duration:1}}
+      >
+        <p className="text-5xl font-outfit mt-36 m-4">
+          Hey</p>
+        <p className="text-4xl font-outfit mt-20 m-4">
+          Welcome to <br/> your personal <br/> medical care vault</p>
+        <p className="text-2xl font-outfit mt-10 ml-4 text-white/40">
+        Your health, our priority</p>
+        <p className="text-2xl font-outfit ml-4 text-white/40">
+        Your journey, our support</p>
+      
+      
+      </motion.div>
+      <motion.div
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{type:"tween",duration:1}}
+      >
         <Image className="absolute left-36 top-20 scale-150 -z-10" 
         src="files2.svg" width={320} height={300}></Image>
-      </div>
-      <div className="bg-white w-44 h-44 rounded-full mt-10 relative -left-10 -bottom-8">
+      </motion.div>
+      
+      <motion.div 
+        initial={{scale:0.5,rotate:-180}}
+        animate={{scale:1,rotate:0}}
+        transition={{duration:0.8}}
+      
+      className="bg-white w-44 h-44 rounded-full mt-10 relative -left-10 -bottom-8">
         <Image src="right.svg" width={50} height={50} className="absolute left-16 top-8"></Image>
-      </div>
+      </motion.div>
     </div>
   )
 }
