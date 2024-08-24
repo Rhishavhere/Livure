2c031515d8f199c1a4747ebf16d443aa1a701475
Rhishav <rhishh.me@gmail.com>
2024-08-24 13:03:06 +0530

    intro page done

--- app/layout.js
+++ app/layout.js
@@ -7,13 +7,18 @@ export const metadata = {
   description: "Your Everyday Health App",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: "no",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
-      </head>
-      <body className="bg-violet-600 overflow-x-hidden">{children}</body>
+      <body className="bg-violet-600 overflow-hidden">{children}</body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -4,18 +4,24 @@ import Image from "next/image"
 
 export default function IntroPage() {
   return (
-    <>
+    <div className="overflow-hidden">
       <div className="flex items-center bg-white/30 w-max p-2 rounded-xl gap-4 m-4">
         {/* <Image src='logo.svg' width={30} height={30} /> */}
         <p className="text-2xl font-outfit">Livure</p>
       </div>
       <div>
-        <p>Hi People</p>
+        <p className="text-5xl font-outfit mt-36 m-4">Hey</p>
+        <p className="text-4xl font-outfit mt-20 m-4">Welcome to <br/> your personal <br/> medical care vault</p>
+        <p className="text-2xl font-outfit mt-10 ml-4 text-white/40">Your health, our priority</p>
+        <p className="text-2xl font-outfit ml-4 text-white/40">Your journey, our support</p>
       </div>
       <div>
-        <Image className="absolute left-20 top-20 scale-150" 
-        src="files2.svg" width={500} height={500}></Image>
+        <Image className="absolute left-36 top-20 scale-150 -z-10" 
+        src="files2.svg" width={320} height={300}></Image>
       </div>
-    </>
+      <div className="bg-white w-44 h-44 rounded-full mt-10 relative -left-10 -bottom-8">
+        <Image src="right.svg" width={50} height={50} className="absolute left-16 top-8"></Image>
+      </div>
+    </div>
   )
 }
